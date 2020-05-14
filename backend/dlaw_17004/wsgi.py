ea54d76aa7b047e7f2444d3f8163775ec12e4560
Crowdbotics <team@crowdbotics.com>
2020-05-14 08:19:56 +0000

    Update models

--- backend/dlaw_17004/wsgi.py
+++ backend/dlaw_17004/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dlaw_17004.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dlaw_17004.settings")
 
 application = get_wsgi_application()
